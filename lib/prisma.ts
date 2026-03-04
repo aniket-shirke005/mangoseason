e5dff4cb5c8b52b55216747c931d02eddb097972
aniket-shirke005 <aniketbshirke07@gmail.com>
2026-03-04 13:18:21 +0530

    prisma config change

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,9 @@
-import { PrismaClient } from "../lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config";
+import { PrismaClient } from "@/lib/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-});
+})
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
